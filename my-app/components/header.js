f42fa9464058596f1d1a6c8d6cd4250587b182f7
keiikoo <e.galaguz@inbox.ru>
2024-10-21 13:35:25 +0300

    first commit

---
+++ my-app/components/header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function Header() {
+    return (
+        <View style={styles.main}>
+            <Text style={styles.text}>СПИСОК ДЕЛ</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    main: {
+        backgroundColor: '#ED254E',
+        display: "flex",
+        height: "100px",
+        alignItems: "center",
+    },
+
+    text: {
+        paddingTop: '50px',
+        fontFamily: 'Helvetica',
+        fontSize: '24px',
+        fontWeight: '700'
+    },
+})
